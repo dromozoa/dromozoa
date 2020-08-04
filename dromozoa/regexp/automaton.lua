54fef555c91d9d212ea422852b8b3ab6f3e2e298
Tomoyuki Fujimori <moyu@dromozoa.com>
2020-08-04 17:22:41 +0900

    正規表現のコードを修正。

--- dromozoa/regexp/automaton.lua
+++ dromozoa/regexp/automaton.lua
@@ -356,65 +356,55 @@ do
   end
 end
 
-do
-  local function set_product(this, that)
-    local this_max_state = this.max_state
-    local this_transitions = this.transitions
-    local that_max_state = that.max_state
-    local that_transitions = that.transitions
-    local n = this_max_state + 1
+function class.difference(this, that)
+  local this_max_state = this.max_state
+  local this_transitions = this.transitions
+  local that_max_state = that.max_state
+  local that_transitions = that.transitions
+  local that_accept_states = that.accept_states
 
-    local result = new()
-    local result_transitions = result.transitions
+  local n = this_max_state + 1
 
-    for i = 0, this_max_state do
-      for j = 0, that_max_state do
-        local u = i + n * j
-        if u ~= 0 then
-          for byte = 0x00, 0xFF do
-            local x = this_transitions[byte][i]
-            local y = that_transitions[byte][j]
-            if not x then
-              x = 0
-            end
-            if not y then
-              y = 0
-            end
-            local v = x + n * y
-            if v ~= 0 then
-              result_transitions[byte][u] = v
-            end
+  local result = new()
+  local result_transitions = result.transitions
+  local result_accept_states = result.accept_states
+
+  for i = 0, this_max_state do
+    for j = 0, that_max_state do
+      local u = i + n * j
+      if u ~= 0 then
+        for byte = 0x00, 0xFF do
+          local x = this_transitions[byte][i]
+          local y = that_transitions[byte][j]
+          if not x then
+            x = 0
+          end
+          if not y then
+            y = 0
+          end
+          local v = x + n * y
+          if v ~= 0 then
+            result_transitions[byte][u] = v
           end
         end
       end
     end
-
-    result.max_state = this_max_state + n * that_max_state
-    result.start_state = this.start_state + n * this.start_state
-    return result
   end
 
-  function class.set_difference(this, that)
-    local result = set_product(this, that)
-
-    local this_max_state = this.max_state
-    local n = this_max_state + 1
-    local that_max_state = that.max_state
-    local that_accept_states = that.accept_states
-    local result_accept_states = result.accept_states
-
-    for i, accept in pairs(this.accept_states) do
-      result_accept_states[i] = accept
-      for j = 1, that_max_state do
-        if not that_accept_states[j] then
-          local u = i + n * j
-          result_accept_states[u] = accept
-        end
+  for i, accept in pairs(this.accept_states) do
+    result_accept_states[i] = accept
+    for j = 1, that_max_state do
+      if not that_accept_states[j] then
+        local u = i + n * j
+        result_accept_states[u] = accept
       end
     end
-
-    return result
   end
+
+  result.max_state = this_max_state + n * that_max_state
+  result.start_state = this.start_state + n * this.start_state
+
+  return result
 end
 
 do
