e93a6902ecedb598b890989c7755425aa4a9e29f
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-02-10 14:42:41 +0900

    正規表現の実装を修正。

--- ext/dromozoa/regexp/dump_ast.lua
+++ ext/dromozoa/regexp/dump_ast.lua
@@ -15,108 +15,32 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa.  If not, see <http://www.gnu.org/licenses/>.
 
-local function dump_set(set)
+local encode_set = require "dromozoa.regexp.encode_set"
 
-  -- {a,b}...
-  -- なんかいい感じの文字セットを作る
-
-  -- { a, b }
-  -- ...
-
-  local n = 0
-  local b
-  for byte = 0x00, 0xFF do
-    if set[byte] then
-      n = n + 1
-      b = byte
-    end
-  end
-
-  io.stderr:write(n, "\n")
-
-  assert(n > 0)
-  if n == 256 then
-    return "."
-  elseif n == 1 then
-    return string.char(b)
-  end
-
-  if n < 256 then
-    local items = {}
-    local i = 0
-    local a
-    local b
-
-    for byte = 0x00, 0xFF do
-      if set[byte] then
-        local item = items[i]
-        if not item then
-          i = i + 1
-          items[i] = { byte, byte }
-        else
-          local a, b = item[1], item[2]
-          if b + 1 == byte then
-            item[2] = byte
-          else
-            i = i + 1
-            items[i] = { byte, byte }
-          end
-        end
-      end
-    end
-
-    local buffer = { "[" }
-
-    for i = 1, #items do
-      local item = items[i]
-      local a, b = item[1], item[2]
-      if a == b then
-        buffer[#buffer + 1] = string.char(a)
-      else
-        buffer[#buffer + 1] = string.char(a)
-        buffer[#buffer + 1] = "-"
-        buffer[#buffer + 1] = string.char(b)
-      end
-    end
-
-    buffer[#buffer + 1] = "]"
-
-    return table.concat(buffer)
-  end
-end
-
-local function dump_node(out, node, id)
+local function dump(out, node, map, id)
   id = id + 1
-  node.id = id
+  map[node] = id
+
   local operator = node[1]
   if operator == "[" then
-    out:write(("%d [label = \"%s\"];\n"):format(id, dump_set(node[2])))
+    out:write(("%d [label=\"%s\", shape=box];\n"):format(id, encode_set(node[2])))
   else
     out:write(("%d [label = \"%s\"];\n"):format(id, operator))
     for i = 2, #node do
-      id = dump_node(out, node[i], id)
+      local that = node[i]
+      id = dump(out, that, map, id)
+      out:write(("%d -> %d;\n"):format(map[node], map[node[i]]))
     end
   end
+
   return id
 end
 
-local function dump_edge(out, node)
-  local operator = node[1]
-  if operator ~= "[" then
-    for i = 2, #node do
-      local that = node[i]
-      out:write(("%d -> %d;\n"):format(node.id, that.id))
-      dump_edge(out, that)
-    end
-  end
-end
-
 return function (out, node)
+  local map = {}
+
   out:write "digraph {\n"
-
-  dump_node(out, node, 0)
-  dump_edge(out, node)
-
+  dump(out, node, map, 0)
   out:write "}\n"
 
   return out
