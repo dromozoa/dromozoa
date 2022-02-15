4f5760bd9fdf774bff1c1b142414c7623db9aa7a
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-02-15 13:04:22 +0900

    遷移アクションを実装。

--- dromozoa/regexp/nfa_to_dfa.lua
+++ dromozoa/regexp/nfa_to_dfa.lua
@@ -69,12 +69,14 @@ local function visit(useq, new_states, epsilon_closures, state_indices, color)
   local new_transition_map = {}
   for byte = 0x00, 0xFF do
     local vmap = {}
+    local action
     for i = 1, #useq do
       local transitions = useq[i].state.transitions
       for j = 1, #transitions do
         local transition = transitions[j]
         local set = transition.set
         if set and set[byte] then
+          action = merge_priority(action, transition.action)
           local vseq = epsilon_closure(transition.v, epsilon_closures, state_indices)
           for k = 1, #vseq do
             local vobj = vseq[k]
@@ -91,9 +93,12 @@ local function visit(useq, new_states, epsilon_closures, state_indices, color)
         vnew = { state = graph.new_state(), seq = vseq }
         new_states[vkey] = vnew
       end
-      local new_transition = new_transition_map[vnew]
+      -- 遷移文字と遷移アクションと遷移先状態でいいかんじにまとめたい
+      -- 遷移先状態と遷移アクションでキーをつくる
+      local new_transition_key = vkey .. ";" .. (action or 0)
+      local new_transition = new_transition_map[new_transition_key]
       if not new_transition then
-        new_transition_map[vnew] = { index = byte, v = vnew, set = { [byte] = true } }
+        new_transition_map[new_transition_key] = { index = byte, v = vnew, set = { [byte] = true }, action = action }
       else
         new_transition.set[byte] = true
       end
@@ -114,7 +119,8 @@ local function visit(useq, new_states, epsilon_closures, state_indices, color)
     local vobj = new_transition.v
     local vnew = vobj.state
     local vseq = vobj.seq
-    graph.new_transition(unew, vnew, new_transition.set)
+    local tnew = graph.new_transition(unew, vnew, new_transition.set)
+    tnew.action = new_transition.action
 
     local accept
     for i = 1, #vseq do
