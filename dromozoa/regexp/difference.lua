b4d3cba2acf06d0ba9b257f1d6ffc973594d9c66
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-03-18 16:57:42 +0900

    リファクタリング。

--- dromozoa/regexp/difference.lua
+++ dromozoa/regexp/difference.lua
@@ -46,62 +46,58 @@ end
 
 local function execute_transition(u, byte)
   if u then
-    return fsm.execute_transition(u, byte)
+    local transition = fsm.execute_transition(u, byte)
+    if transition then
+      return transition, transition.v, transition.action
+    end
   end
 end
 
-return function (this, that)
-  local this_states, this_indices = create_states_and_indices(this)
-  local that_states, that_indices = create_states_and_indices(that)
+return function (u, v)
+  local u_states, u_indices = create_states_and_indices(u)
+  local v_states, v_indices = create_states_and_indices(v)
 
-  local n = #this_states + 1
+  local un = #u_states
+  local vn = #v_states
+  local n = un + 1
 
   local new_states = {}
 
-  for i = 0, #this_states do
-    local ux = this_states[i]
-
-    for j = 0, #that_states do
-      local uy = that_states[j]
-
-      -- ux, uy
-      -- x, y
-      -- vx, vy
-      -- ukey, unew
-      -- vkey, vnew
-
+  for i = 0, un do
+    local ux = u_states[i]
+    for j = 0, vn do
+      local uy = v_states[j]
       local ukey = i + j * n
       if ukey ~= 0 then
-
         local action_index = 0
         local actions = {}
         local new_transition_map = {}
 
         for byte = 0x00, 0xFF do
-          local x = execute_transition(ux, byte)
-          local y = execute_transition(uy, byte)
-          local vx
-          local vy
+          local tx, vx, action = execute_transition(ux, byte)
+          local ty, vy = execute_transition(uy, byte)
+
           local vkey = 0
-          if x then
-            vx = x.v
-            vkey = this_indices[vx]
+          local timestamp
+          if tx then
+            vkey = u_indices[vx]
+            timestamp = tx.timestamp
+            if ty then
+              vkey = vkey + v_indices[vy] * n
+            end
+          elseif ty then
+            vkey = v_indices[vy] * n
+            timestamp = ty.timestamp
           end
-          if y then
-            vy = y.v
-            vkey = vkey + that_indices[vy] * n
-          end
+
           if vkey ~= 0 then
             local unew = new_states[ukey]
             if not unew then
               unew = fsm.new_state()
               new_states[ukey] = unew
-
-              if ux and ux.accept then
-                if not uy or not uy.accept then
-                  unew.accept = ux.accept
-                  unew.timestamp = ux.timestamp
-                end
+              if ux and ux.accept and (not uy or not uy.accept) then
+                unew.accept = ux.accept
+                unew.timestamp = ux.timestamp
               end
             end
             local vnew = new_states[vkey]
@@ -109,39 +105,24 @@ return function (this, that)
               vnew = fsm.new_state()
               new_states[vkey] = vnew
 
-              if vx and vx.accept then
-                if not vy or not vy.accept then
-                  vnew.accept = vx.accept
-                  vnew.timestamp = vx.timestamp
-                end
+              if vx and vx.accept and (not vy or not vy.accept) then
+                vnew.accept = vx.accept
+                vnew.timestamp = vx.timestamp
               end
             end
 
-            local action
-            local timestamp
             local new_transition_key
-            if x then
-              action = x.action
-              timestamp = x.timestamp
-              if action then
-                local index = actions[action]
-                if not index then
-                  action_index = action_index + 1
-                  actions[action] = action_index
-                  new_transition_key = vkey .. ";" .. action_index
-                else
-                  new_transition_key = vkey .. ";" .. index
-                end
+            if action then
+              local index = actions[action]
+              if not index then
+                action_index = action_index + 1
+                actions[action] = action_index
+                new_transition_key = vkey .. ";" .. action_index
               else
-                new_transition_key = vkey
+                new_transition_key = vkey .. ";" .. index
               end
             else
               new_transition_key = vkey
-            end
-            if y then
-              if not timestamp then
-                timestamp = y.timestamp
-              end
             end
 
             local new_transition = new_transition_map[new_transition_key]
@@ -163,9 +144,9 @@ return function (this, that)
     end
   end
 
-  local uid = this_indices[this] + n * that_indices[that]
+  local uid = u_indices[u] + n * v_indices[v]
   local u = new_states[uid]
-  u.timestamp = this.timestamp
+  u.timestamp = u.timestamp
 
   -- remove dead states
   remove_dead_states(u)
