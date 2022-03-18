ecad628fa7cf082a83d973954d32e366b4142cd8
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-03-18 19:54:29 +0900

    リファクタリング。

--- dromozoa/regexp/compile.lua
+++ dromozoa/regexp/compile.lua
@@ -78,7 +78,7 @@ local template2 = [[
   end
 
   while true do
-    local guard_action = _[current_index].guard_action
+    local guard_action = _[current_index].guard
     local guarded
     if current_state == _[current_index].start_state and guard_action then
       -- guardを評価する
@@ -217,14 +217,14 @@ return function(out, data)
   out:write(template1)
 
   local compactor = { index = 0 }
-  local guard_action = {}
+  local guard = {}
   local accept_actions = {}
   local transition_actions = {}
 
   out:write "local c={\n"
   for i = 1, n do
     local item = data[i]
-    guard_action[i] = dump_action(out, item.guard_action, compactor)
+    guard[i] = dump_action(out, item.guard, compactor)
     accept_actions[i] = dump_actions(out, item.accept_actions, compactor)
     transition_actions[i] = dump_actions(out, item.transition_actions, compactor)
   end
@@ -235,7 +235,7 @@ return function(out, data)
     local item = data[i]
     out:write "{\n"
     out:write("loop=", item.loop and "true" or "false", ";\n")
-    out:write("guard_action=", guard_action[i], ";\n")
+    out:write("guard=", guard[i], ";\n")
     out:write("max_accept_state=", item.max_accept_state, ";\n")
     out:write("accept_actions=", accept_actions[i], ";\n")
     out:write("max_transition=", item.max_transition, ";\n")
