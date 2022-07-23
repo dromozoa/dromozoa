1a5cb0e7f22ec72e57f36668de9938f780bc16a2
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-07-23 13:03:20 +0900

    テストを修正。

--- test/test_regexp.lua
+++ test/test_regexp.lua
@@ -504,9 +504,214 @@ end
 
 ---------------------------------------------------------------------------
 
+local assertion = false
 
+local function visit(u, accept_partition_map, nonaccept_partition, partition_map, color)
+  color[u] = 1
 
+  local accept_action = u.accept_action
+  local partition = nonaccept_partition
+  if accept_action then
+    partition = accept_partition_map[accept_action]
+    if not partition then
+      partition = { timestamp = u.timestamp }
+      accept_partition_map[accept_action] = partition
+    else
+      local t = u.timestamp
+      if partition.timestamp > t then
+        partition.timestamp = t
+      end
+    end
+  end
+  partition[#partition + 1] = u
+  partition_map[u] = partition
 
+  local transitions = u.transitions
+  for i = 1, #transitions do
+    local transition = transitions[i]
+    local v = transition.v
+    if not color[v] then
+      visit(v, accept_partition_map, nonaccept_partition, partition_map, color)
+    end
+  end
+
+  color[u] = 2
+end
+
+local function create_initial_partitions(u)
+  local accept_partition_map = {}
+  local nonaccept_partition = {}
+  local partition_map = {}
+  visit(u, accept_partition_map, nonaccept_partition, partition_map, {})
+
+  local partitions = {}
+  for _, partition in pairs(accept_partition_map) do
+    partitions[#partitions + 1] = partition
+  end
+  table.sort(partitions, function (a, b) return a.timestamp < b.timestamp end)
+
+  if #nonaccept_partition > 0 then
+    partitions[#partitions + 1] = nonaccept_partition
+  end
+
+  return partitions, partition_map
+end
+
+local function execute_transition(u, byte)
+  local transition = u:execute_transition(byte)
+  if transition then
+    return transition.v, transition.action
+  end
+end
+
+function module.minimize(u)
+  local partitions, partition_map = create_initial_partitions(u)
+
+  while true do
+    local new_partitions = {}
+    local new_partition_map = {}
+
+    for i = 1, #partitions do
+      local partition = partitions[i]
+      -- あるパーティションに含まれる状態の組 (x, y) が同じ遷移をするならば、ひ
+      -- とつのパーティションにまとめる。
+      for j = 1, #partition do
+        local x = partition[j]
+        for k = 1, j - 1 do
+          local y = partition[k]
+          -- 全ての文字について下記の条件が満たされたら、同じ遷移をするとみなす。
+          -- 1. 遷移先の状態が同じパーティションに含まれている
+          -- 2. 同じ遷移アクションを持つ
+          local same_partition = true
+          for byte = 0x00, 0xFF do
+            local xv, xaction = execute_transition(x, byte)
+            local yv, yaction = execute_transition(y, byte)
+            if partition_map[xv] ~= partition_map[yv] or not rawequal(xaction, yaction) then
+              same_partition = false
+              break
+            end
+          end
+
+          if same_partition then
+            local new_partition = new_partition_map[x]
+            if not new_partition then
+              local new_partition = new_partition_map[y]
+              new_partition[#new_partition + 1] = x
+              new_partition_map[x] = new_partition
+            else
+              -- 新パーティションに登録済みである
+              if assertion then
+                assert(new_partition == new_partition_map[y])
+              end
+            end
+          end
+        end
+
+        if not new_partition_map[x] then
+          local new_partition = { x }
+          new_partitions[#new_partitions + 1] = new_partition
+          new_partition_map[x] = new_partition
+        end
+      end
+    end
+
+    if #partitions == #new_partitions then
+      break
+    end
+
+    partitions = new_partitions
+    partition_map = new_partition_map
+  end
+
+  local states = {}
+  local accept_states = {}
+  for i = 1, #partitions do
+    local partition = partitions[i]
+
+    local accept_action = partition[1].accept_action
+    local timestamp
+    if accept_action then
+      timestamp = partition[1].timestamp
+      for j = 2, #partition do
+        local x = partition[j]
+        local t = x.timestamp
+        if timestamp > t then
+          timestamp = t
+        end
+      end
+    end
+
+    local unew = module.state()
+    unew.accept_action = accept_action
+    unew.timestamp = timestamp
+    states[partition] = { key = i, state = unew }
+    if accept_action then
+      accept_states[#accept_states + 1] = unew
+    end
+  end
+
+  for i = 1, #partitions do
+    local partition = partitions[i]
+    local unew = states[partition].state
+
+    local actions = {}
+    local new_transition_map = {}
+
+    for byte = 0x00, 0xFF do
+      local transition = partition[1]:execute_transition(byte)
+
+      if transition then
+        local timestamp = transition.timestamp
+        local action = transition.action
+        local v = states[partition_map[transition.v]]
+        local vkey = v.key
+        local vnew = v.state
+
+        -- パーティションに含まれる各状態は同じ遷移をする
+        for j = 2, #partition do
+          local transition = partition[j]:execute_transition(byte)
+          if assertion then
+            assert(rawequal(action, transition.action))
+            assert(vkey == states[partition_map[transition.v]].key)
+            assert(vnew == states[partition_map[transition.v]].state)
+          end
+          local t = transition.timestamp
+          if timestamp > t then
+            timestamp = t
+          end
+        end
+
+        local new_transition_key = module.new_transition_key(vkey, actions, action)
+        local new_transition = new_transition_map[new_transition_key]
+        if not new_transition then
+          new_transition = module.transition(unew, vnew, { [byte] = true })
+          new_transition.action = action
+          new_transition.timestamp = timestamp
+          new_transition_map[new_transition_key] = new_transition
+        else
+          if assertion then
+            assert(rawequal(action, new_transition.action))
+            assert(vnew == new_transition.v)
+          end
+          new_transition.set[byte] = true
+          if new_transition.timestamp > timestamp then
+            new_transition.timestamp = timestamp
+          end
+        end
+      else
+        if assertion then
+          for j = 2, #partition do
+            assert(not partition[j]:execute_transition(byte))
+          end
+        end
+      end
+    end
+  end
+
+  local unew = states[partition_map[u]].state
+  unew.timestamp = u.timestamp
+  return unew, accept_states
+end
 
 ---------------------------------------------------------------------------
 
@@ -531,6 +736,7 @@ local x
 -- print(dumper.encode(x, { pretty = true, stable = true }))
 local n = module.tree_to_nfa(x, true)
 local d = module.nfa_to_dfa(n)
+local e = module.minimize(d)
 
 local out = assert(io.open("test-tree.dot", "w"))
 write_graphviz_tree(out, x)
@@ -540,8 +746,12 @@ local out = assert(io.open("test-nfa.dot", "w"))
 write_graphviz(out, n)
 out:close()
 
-local out = assert(io.open("test-dfa.dot", "w"))
+local out = assert(io.open("test-dfa1.dot", "w"))
 write_graphviz(out, d)
+out:close()
+
+local out = assert(io.open("test-dfa2.dot", "w"))
+write_graphviz(out, e)
 out:close()
 
 
