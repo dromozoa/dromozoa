84c9a4647f59459982b0ec53d91c6fc82dc9111d
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-07-28 19:58:41 +0900

    patternを反映。

--- test/test_regexp.lua
+++ test/test_regexp.lua
@@ -19,202 +19,9 @@ local dumper = require "dromozoa.commons.dumper"
 local write_graphviz = require "dromozoa.regexp.write_graphviz"
 local compare = require "dromozoa.compare"
 local tree_map = require "dromozoa.tree_map"
+local pattern = require "dromozoa.regexp.pattern"
 
 local module = {}
-
----------------------------------------------------------------------------
-
-local metatable = { __name = "dromozoa.regexp.pattern" }
-
-local timestamp = 0
-
-local function construct(code, ...)
-  timestamp = timestamp + 1
-  return setmetatable({ timestamp = timestamp, [0] = code, ... }, metatable)
-end
-
-local function pattern(that)
-  if type(that) == "string" then
-    local self = construct("[", { [that:byte(1)] = true })
-    for i = 2, #that do
-      self = self + construct("[", { [that:byte(i)] = true })
-    end
-    self.name = that
-    return self
-  else
-    return that
-  end
-end
-
-local function range(that)
-  if type(that) == "string" then
-    local set = {}
-    for i = 1, #that, 2 do
-      local a, b = that:byte(i, i + 1)
-      if b == nil then
-        b = a
-      end
-      for byte = a, b do
-        set[byte] = true
-      end
-    end
-    return construct("[", set)
-  else
-    return pattern(that)
-  end
-end
-
-local function set(that)
-  if type(that) == "string" then
-    local set = {}
-    for i = 1, #that do
-      set[that:byte(i)] = true
-    end
-    return construct("[", set)
-  else
-    return pattern(that)
-  end
-end
-
-local function union(self, that)
-  local self = pattern(self)
-  local that = pattern(that)
-  if self[0] == "%" or that[0] == "%" then
-    error "not supported"
-  elseif self[0] == "[" and that[0] == "[" then
-    local set = {}
-    for byte in pairs(self[1]) do
-      set[byte] = true
-    end
-    for byte in pairs(that[1]) do
-      set[byte] = true
-    end
-    return construct("[", set)
-  else
-    return construct("|", self, that)
-  end
-end
-
-function metatable:__add(that)
-  local self = pattern(self)
-  local that = pattern(that)
-  if self[0] == "%" or that[0] == "%" then
-    error "not supported"
-  else
-    return construct(".", self, that)
-  end
-end
-
-function metatable:__sub(that)
-  local self = pattern(self)
-  local that = pattern(that)
-  if self[0] == "%" or that[0] == "%" then
-    error "not supported"
-  elseif self[0] == "[" and that[0] == "[" then
-    local sub = that[1]
-    local set = {}
-    for byte in pairs(self[1]) do
-      if not sub[byte] then
-        set[byte] = true
-      end
-    end
-    return construct("[", set)
-  else
-    return construct("-", self, that)
-  end
-end
-
-function metatable:__div(that)
-  local self = pattern(self)
-  if self[0] == "[" then
-    return construct("/", self, that)
-  else
-    error "not supported"
-  end
-end
-
-function metatable:__mod(that)
-  local self = pattern(self)
-  if self[0] == "%" then
-    error "not supported"
-  else
-    local result = construct("%", self, that)
-    result.name = self.name
-    return result
-  end
-end
-
-function metatable:__unm()
-  if self[0] == "[" then
-    local neg = self[1]
-    local set = {}
-    for byte = 0x00, 0xFF do
-      if not neg[byte] then
-        set[byte] = true
-      end
-    end
-    return construct("[", set)
-  else
-    error "not supported"
-  end
-end
-
-function metatable:__call(that)
-  if self[0] == "%" then
-    error "not supported"
-  else
-    local m = that[1]
-    local n = that[2]
-    if n == nil then
-      if m == 0 then
-        return construct("*", self)
-      elseif m == 1 then
-        return construct("+", self)
-      else
-        local result = self
-        for i = 3, m do
-          result = result + self
-        end
-        return result + construct("+", self)
-      end
-    else
-      if m == 0 then
-        local result = construct("?", self)
-        for i = 2, n do
-          result = result + construct("?", self)
-        end
-        return result
-      else
-        local result = self
-        for i = 2, m do
-          result = result + self
-        end
-        for i = m + 1, n do
-          result = result + construct("?", self)
-        end
-        return result
-      end
-    end
-  end
-end
-
-module.pattern = setmetatable({}, {
-  __index = function (_, that)
-    return range(that)
-  end;
-
-  __call = function (_, that)
-    if type(that) == "table" and getmetatable(that) ~= metatable then
-      local result = set(that[1])
-      for i = 2, #that do
-        result = union(result, set(that[i]))
-      end
-      return result
-    else
-      return pattern(that)
-    end
-  end;
-})
 
 ---------------------------------------------------------------------------
 
@@ -290,7 +97,7 @@ local function node_to_nfa(node)
   if code == "[" then
     local u = state()
     local v = state()
-    transition(u, v, node[1], timestamp)
+    transition(u, v, node[1], node.timestamp)
     return u, v
   else
     local au, av = node_to_nfa(node[1])
@@ -328,8 +135,8 @@ local function node_to_nfa(node)
           transition(av, v)
           transition(bv, v)
         elseif code == "-" then
-          av:update(timestamp, "")
-          bv:update(timestamp, "")
+          av:update(node.timestamp, "")
+          bv:update(node.timestamp, "")
           local cu, accept_states = difference(au, bu)
           transition(u, cu)
           for _, cv in ipairs(accept_states) do
@@ -781,7 +588,7 @@ end
 
 ---------------------------------------------------------------------------
 
-local _ = module.pattern
+local _ = pattern
 
 ---------------------------------------------------------------------------
 
