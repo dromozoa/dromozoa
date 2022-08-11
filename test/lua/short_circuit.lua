874b6eace55c7cfb9daec135dd6745625d16d7bf
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-08-11 19:25:35 +0900

    テストを追加。

---
+++ test/lua/short_circuit.lua
@@ -0,0 +1,34 @@
+-- Copyright (C) 2022 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa.
+--
+-- dromozoa is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa.  If not, see <http://www.gnu.org/licenses/>.
+
+local function f() end
+
+local function a()
+  return f() and f() and f()
+end
+
+local function b()
+  return f() and f() or f()
+end
+
+local function b()
+  return f() or f() and f()
+end
+
+local function b()
+  return f() or f() or f()
+end
