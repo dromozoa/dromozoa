2a2137407179dc6bd5027284ff9e983c86962fd3
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-09-02 12:12:08 +0900

    単純な命令を追加。

--- test/run/binop.lua
+++ test/run/binop.lua
@@ -15,55 +15,18 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa.  If not, see <http://www.gnu.org/licenses/>.
 
-local function f1()
-  io.write "f1\n"
+local a = 10 % -4
+if a < 0 then
+  io.write "OK\n"
+else
+  io.write "NG\n"
 end
 
-local function f2(a, b)
-  io.write "f2\n"
-  io.write(a)
-  io.write(b)
+local a = 10 // -4
+if a == -3 then
+  io.write "OK\n"
+else
+  io.write "NG\n"
 end
 
-f1()
-f2("foo\n", "bar\n")
-f1()
-
-local t = { "a", "b", "c", "d" }
-local function f(t, i)
-  i = i + 1
-  local v = t[i]
-  if v == nil then
-    return
-  else
-    return i, v
-  end
-end
-
-for i, v in f, t, 0 do
-  io.write(v)
-  io.write "\n"
-end
-
-for i, v in f, t, 0 do
-  if i == 3 then
-    break
-  end
-  io.write(v)
-  io.write "\n"
-end
-
-for i = 1, 4 do
-  io.write(t[i])
-  io.write "\n"
-end
-
-t.f = function (a)
-  if a then
-    io.write "t.f(a)\n"
-  else
-    io.write "t.f()\n"
-  end
-end
-t.f()
-t:f()
+io.write("OK " .. a .. "\n")
--- test_lua54.lua
+++ test_lua54.lua
@@ -931,7 +931,7 @@ local function generate_proto_code(out, protos, u, n)
 
   elseif u_name == "if" then
     out:write "a=S.pop();"
-    out:write "if (a !== undefined && a !== false) {\n"
+    out:write "if (a!==undefined&&a!==false) {\n"
     for _, v in ipairs(u[1]) do
       generate_proto_code(out, protos, v, n)
     end
@@ -948,40 +948,30 @@ local function generate_proto_code(out, protos, u, n)
     end
     out:write(("  "):rep(n), "} while (true);")
 
-  elseif u_name == "add" then
-    out:write "b=S.pop();"
-    out:write "a=S.pop();"
-    out:write "S.push(a+b);"
+  elseif u_name == "add" then out:write "b=S.pop();a=S.pop();S.push(a+b);"
+  elseif u_name == "sub" then out:write "b=S.pop();a=S.pop();S.push(a-b);"
+  elseif u_name == "mul" then out:write "b=S.pop();a=S.pop();S.push(a*b);"
+  elseif u_name == "div" then out:write "b=S.pop();a=S.pop();S.push(a/b);"
+  elseif u_name == "idiv" then out:write "b=S.pop();a=S.pop();S.push(Math.floor(a/b));"
+  elseif u_name == "mod" then out:write "b=S.pop();a=S.pop();S.push((a%b+b)%b);"
+  elseif u_name == "pow" then out:write "b=S.pop();a=S.pop();S.push(Math.pow(a,b));"
+  elseif u_name == "band" then out:write "b=S.pop();a=S.pop();S.push(a&b);"
+  elseif u_name == "bxor" then out:write "b=S.pop();a=S.pop();S.push(a^b);"
+  elseif u_name == "bor" then out:write "b=S.pop();a=S.pop();S.push(a|b);"
+  elseif u_name == "shr" then out:write "b=S.pop();a=S.pop();S.push(a>>b);"
+  elseif u_name == "shl" then out:write "b=S.pop();a=S.pop();S.push(a<<b);"
+  elseif u_name == "concat" then out:write "b=S.pop();a=S.pop();S.push(a.toString()+b.toString());"
+  elseif u_name == "lt" then out:write "b=S.pop();a=S.pop();S.push(a<b);"
+  elseif u_name == "le" then out:write "b=S.pop();a=S.pop();S.push(a<=b);"
+  elseif u_name == "gt" then out:write "b=S.pop();a=S.pop();S.push(a>b);"
+  elseif u_name == "ge" then out:write "b=S.pop();a=S.pop();S.push(a>=b);"
+  elseif u_name == "eq" then out:write "b=S.pop();a=S.pop();S.push(a===b);"
+  elseif u_name == "ne" then out:write "b=S.pop();a=S.pop();S.push(a!==b);"
 
-  elseif u_name == "lt" then
-    out:write "b=S.pop();"
-    out:write "a=S.pop();"
-    out:write "S.push(a<b);"
-
-  elseif u_name == "le" then
-    out:write "b=S.pop();"
-    out:write "a=S.pop();"
-    out:write "S.push(a<=b);"
-
-  elseif u_name == "gt" then
-    out:write "b=S.pop();"
-    out:write "a=S.pop();"
-    out:write "S.push(a>b);"
-
-  elseif u_name == "ge" then
-    out:write "b=S.pop();"
-    out:write "a=S.pop();"
-    out:write "S.push(a>=b);"
-
-  elseif u_name == "eq" then
-    out:write "b=S.pop();"
-    out:write "a=S.pop();"
-    out:write "S.push(a===b);"
-
-  elseif u_name == "ne" then
-    out:write "b=S.pop();"
-    out:write "a=S.pop();"
-    out:write "S.push(a!==b);"
+  elseif u_name == "unm" then out:write "a=S.pop();S.push(-a);"
+  elseif u_name == "not" then out:write "a=S.pop();S.push(a===undefined||a===false);"
+  elseif u_name == "len" then out:write "b=1;a=S.pop();for(;a.get(b)!==undefined;++b);S.push(b-1);"
+  elseif u_name == "bnot" then out:write "a=S.pop();S.push(~a);"
 
   elseif u_name == "new_local" then
     out:write("V", a, "=[S.pop()];")
