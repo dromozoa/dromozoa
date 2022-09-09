3db0ca8ed5715154d32ec624069fb2e15bb584ab
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-09-09 13:59:50 +0900

    リファクタリング。

--- dromozoa/regexp/compile.lua
+++ dromozoa/regexp/compile.lua
@@ -15,7 +15,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa.  If not, see <http://www.gnu.org/licenses/>.
 
-local tree_set = require "dromozoa.tree_set"
 local runtime = require "dromozoa.regexp.runtime"
 
 -- TODO machineと共通？
