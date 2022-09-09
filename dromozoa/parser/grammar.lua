b652498392fb05a2413392ff35999806d9e15036
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-09-09 14:29:21 +0900

    リファクタリング。

--- dromozoa/parser/grammar.lua
+++ dromozoa/parser/grammar.lua
@@ -15,7 +15,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa.  If not, see <http://www.gnu.org/licenses/>.
 
-local array = require "dromozoa.array"
 local tree_map = require "dromozoa.tree_map"
 local tree_set = require "dromozoa.tree_set"
 
