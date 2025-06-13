0556b124ee2fe4841358638f42b20a4862f612a8
Tomoyuki Fujimori <moyu@dromozoa.com>
2025-06-14 07:30:30 +0900

    著作権表記を更新。

--- boot2.lua
+++ boot2.lua
@@ -13,7 +13,7 @@
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with dromozoa.  If not, see <http://www.gnu.org/licenses/>.
+-- along with dromozoa.  If not, see <https://www.gnu.org/licenses/>.
 
 function lexer(source)
   write_string(source)
