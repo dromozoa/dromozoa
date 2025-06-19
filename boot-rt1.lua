ca6f145bb700390260ab7d493c2003d6bd16d8a5
Tomoyuki Fujimori <moyu@dromozoa.com>
2025-06-19 10:24:59 +0900

    ランタイムを更新。

--- boot-rt1.lua
+++ boot-rt1.lua
@@ -15,9 +15,75 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa.  If not, see <https://www.gnu.org/licenses/>.
 
-function __new(n)
+function __roundup(n, a)
+  local r = n % a
+  if r == 0 then
+    return n
+  else
+    return n + a - r
+  end
 end
 
-function dump(v)
-  -- noop
+function __new(n)
+  local pointer = __heap_pointer
+  __heap_pointer = pointer + __roundup(n, 8)
+
+  local memory_size = __memory_size() * 65536
+  if __heap_pointer >= memory_size then
+    __memory_grow(__roundup(memory_size, 65536))
+  end
+
+  return pointer
+end
+
+function __pack_string(size, data)
+  local this = __new(8)
+  i32_store(this, size)
+  i32_store(this + 4, data)
+  return this
+end
+
+function __unpack_string(s)
+  local size = i32_load(s)
+  local data = i32_load(s)
+  return size, data
+end
+
+function integer_to_string(v)
+  local b = new(16)
+  local p = b + 15
+  i32_store8(p, 0x00)
+
+  local neg = v < 0
+  if neg then
+    v = -v
+  end
+
+  repeat
+    local r = v % 10
+    v = v // 10
+    p = p - 1
+    i32_store8(p, r + 0x30)
+  until v == 0
+
+  if neg then
+    p = p - 1
+    i32_store8(p, 0x2D)
+  end
+
+  return __pack_string(b + 15 - p, p)
+end
+
+function io_write_string(s)
+  local size, data = __unpack_string(s)
+  local item = __new(8)
+  i32_store(item, data)
+  i32_store(item + 4, size)
+  local out = __new(4)
+  i32_store(out, 0)
+  fd_write(1, item, 1, out)
+end
+
+function io_write_integer(v)
+  io_write_string(integer_to_string(v))
 end
--- test2.lua
+++ test2.lua
@@ -1,18 +1,7 @@
 local A = 42
 local B = 17
 
-
-function main(x, a)
-  local a, b = main(17, a)
-
-  for i = 1, 10 do
-    a = 12
-    local v = a * b * i
-    break
-  end
-
-  x = ('foooooooooooooooo')
-  return A, b, b
+function main()
 end
 
--- export_start(main)
+__export_start(main)
