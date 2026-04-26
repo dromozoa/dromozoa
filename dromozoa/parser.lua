e5b05878f0672b920d3344acec72310f795dae71
Tomoyuki Fujimori <moyu@dromozoa.com>
2026-04-26 15:03:07 +0900

    パーサを開発中。

--- dromozoa/parser.lua
+++ dromozoa/parser.lua
@@ -15,15 +15,21 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa.  If not, see <https://www.gnu.org/licenses/>.
 
-local left_denotation = require "dromozoa.left_denotation"
 local node = require "dromozoa.node"
-local null_denotation = require "dromozoa.null_denotation"
 
----@type table<string, dromozoa.null_denotation>
-local nud_table = {}
+---@alias dromozoa.nud fun(parser: dromozoa.parser, token: dromozoa.token): dromozoa.node
+---@alias dromozoa.led_function fun(parser: dromozoa.parser, left: dromozoa.node, token: dromozoa.token, rbp: integer): dromozoa.node
+---@alias dromozoa.led { lbp: integer, fn: dromozoa.led_function }
 
----@type table<string, dromozoa.left_denotation>
-local led_table = {}
+---@type table<string, dromozoa.nud>
+local prefixexp_nuds = {}
+---@type table<string, dromozoa.led>
+local prefixexp_leds = {}
+
+---@type table<string, dromozoa.nud>
+local exp_nuds = {}
+---@type table<string, dromozoa.led>
+local exp_leds = {}
 
 ---@class dromozoa.parser
 ---@field tokens dromozoa.token[]?
@@ -66,6 +72,26 @@ function class:nud_token(token)
   return node.new(token.kind, token)
 end
 
+function class:nud_function(token)
+  print(token)
+end
+
+function class:nud_name(token)
+  print(token)
+end
+
+function class:nud_group(token)
+  print(token)
+end
+
+function class:nud_table(token)
+  print(token)
+end
+
+function class:nud_prefix(token)
+  print(token)
+end
+
 ---@param left dromozoa.node
 ---@param token dromozoa.token
 ---@param rbp integer
@@ -88,33 +114,48 @@ function class:led_right(left, token, rbp)
   }
 end
 
--- function class:led_call(left, token, rbp)
--- end
---
--- function class:led_subscript(left, token, rbp)
--- end
---
--- function class:led_field(left, token, rbp)
--- end
+function class:led_call(left, token, rbp)
+  print(left, token, rbp)
+end
+
+function class:led_subscript(left, token, rbp)
+  print(left, token, rbp)
+end
+
+function class:led_field(left, token, rbp)
+  print(left, token, rbp)
+end
 
 ---@param rbp integer
-function class:parse_exp(rbp)
+---@param nuds table<string, dromozoa.nud>
+---@param leds table<string, dromozoa.led>
+function class:parse_exp_impl(rbp, nuds, leds)
   local token = self:read()
-  local nud = nud_table[token.kind]
+  local nud = nuds[token.kind]
   if not nud then
     error("parser error at " .. token.srcloc:to_string())
   end
-  local left = nud:nud(self, token)
+  local left = nud(self, token)
   while true do
     local token = self:peek()
-    local led = led_table[token.kind]
+    local led = leds[token.kind]
     if not led or led.lbp <= rbp then
       break
     end
     self:read()
-    left = led:led(self, left, token)
+    left = led.fn(self, left, token, led.lbp)
   end
   return left
+end
+
+---@param rbp integer
+function class:parse_prefixexp(rbp)
+  return self:parse_exp_impl(rbp, prefixexp_nuds, prefixexp_leds)
+end
+
+---@param rbp integer
+function class:parse_exp(rbp)
+  return self:parse_exp_impl(rbp, exp_nuds, exp_leds)
 end
 
 ---@param tokens dromozoa.token[]
@@ -124,42 +165,55 @@ function class:parse(tokens)
   return self:parse_exp(0)
 end
 
--- https://www.lua.org/manual/5.5/manual.html#9
 do
-  nud_table["nil"]     = null_denotation.new(class.nud_token, false)
-  nud_table["false"]   = null_denotation.new(class.nud_token, false)
-  nud_table["true"]    = null_denotation.new(class.nud_token, false)
-  nud_table["Integer"] = null_denotation.new(class.nud_token, false)
+  prefixexp_nuds["Name"] = class.nud_name
+  prefixexp_nuds["("]    = class.nud_group
 end
 
-
--- https://www.lua.org/manual/5.5/manual.html#3.4.8
 do
-  led_table["or"]  = left_denotation.new(100, class.led_left, false)
-  led_table["and"] = left_denotation.new(110, class.led_left, false)
-  led_table["<"]   = left_denotation.new(120, class.led_left, false)
-  led_table[">"]   = left_denotation.new(120, class.led_left, false)
-  led_table["<="]  = left_denotation.new(120, class.led_left, false)
-  led_table[">="]  = left_denotation.new(120, class.led_left, false)
-  led_table["~="]  = left_denotation.new(120, class.led_left, false)
-  led_table["=="]  = left_denotation.new(120, class.led_left, false)
-  led_table["|"]   = left_denotation.new(130, class.led_left, false)
-  led_table["~"]   = left_denotation.new(140, class.led_left, false)
-  led_table["&"]   = left_denotation.new(150, class.led_left, false)
-  led_table["~"]   = left_denotation.new(160, class.led_left, false)
-  led_table["<<"]  = left_denotation.new(170, class.led_left, false)
-  led_table[">>"]  = left_denotation.new(170, class.led_left, false)
-  led_table[".."]  = left_denotation.new(180, class.led_right, false)
-  led_table["+"]   = left_denotation.new(190, class.led_left, false)
-  led_table["-"]   = left_denotation.new(190, class.led_left, false)
-  led_table["*"]   = left_denotation.new(200, class.led_left, false)
-  led_table["/"]   = left_denotation.new(200, class.led_left, false)
-  led_table["//"]  = left_denotation.new(200, class.led_left, false)
-  led_table["%"]   = left_denotation.new(200, class.led_left, false)
-  led_table["^"]   = left_denotation.new(210, class.led_left, false)
-  -- led_table["("]   = left_denotation.new(900, class.led_call, true)
-  -- led_table["["]   = left_denotation.new(900, class.led_subscript, true)
-  -- led_table["."]   = left_denotation.new(900, class.led_field, true)
+  exp_nuds["nil"]      = class.nud_token
+  exp_nuds["false"]    = class.nud_token
+  exp_nuds["true"]     = class.nud_token
+  exp_nuds["Integer"]  = class.nud_token
+  exp_nuds["String"]   = class.nud_token
+  exp_nuds["..."]      = class.nud_token
+  exp_nuds["function"] = class.nud_function
+  exp_nuds["{"]        = class.nud_table
+  exp_nuds["-"]        = class.nud_prefix
+  exp_nuds["not"]      = class.nud_prefix
+  exp_nuds["#"]        = class.nud_prefix
+  exp_nuds["~"]        = class.nud_prefix
+end
+
+do
+  exp_leds["or"]  = { lbp = 100, fn = class.led_left }
+  exp_leds["and"] = { lbp = 110, fn = class.led_left }
+  exp_leds["<"]   = { lbp = 120, fn = class.led_left }
+  exp_leds[">"]   = { lbp = 120, fn = class.led_left }
+  exp_leds["<="]  = { lbp = 120, fn = class.led_left }
+  exp_leds[">="]  = { lbp = 120, fn = class.led_left }
+  exp_leds["~="]  = { lbp = 120, fn = class.led_left }
+  exp_leds["=="]  = { lbp = 120, fn = class.led_left }
+  exp_leds["|"]   = { lbp = 130, fn = class.led_left }
+  exp_leds["~"]   = { lbp = 140, fn = class.led_left }
+  exp_leds["&"]   = { lbp = 150, fn = class.led_left }
+  exp_leds["~"]   = { lbp = 160, fn = class.led_left }
+  exp_leds["<<"]  = { lbp = 170, fn = class.led_left }
+  exp_leds[">>"]  = { lbp = 170, fn = class.led_left }
+  exp_leds[".."]  = { lbp = 180, fn = class.led_right }
+  exp_leds["+"]   = { lbp = 190, fn = class.led_left }
+  exp_leds["-"]   = { lbp = 190, fn = class.led_left }
+  exp_leds["*"]   = { lbp = 200, fn = class.led_left }
+  exp_leds["/"]   = { lbp = 200, fn = class.led_left }
+  exp_leds["//"]  = { lbp = 200, fn = class.led_left }
+  exp_leds["%"]   = { lbp = 200, fn = class.led_left }
+  exp_leds["^"]   = { lbp = 210, fn = class.led_right }
+end
+
+do
+  prefixexp_leds["("] = { lbp = 900, fn = class.led_call }
+  prefixexp_leds["["] = { lbp = 900, fn = class.led_subscript }
+  prefixexp_leds["."] = { lbp = 900, fn = class.led_field }
 end
 
 return class
