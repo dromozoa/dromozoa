13eae1846f90fde4389db54b2dea4b084e9e1189
Tomoyuki Fujimori <moyu@dromozoa.com>
2026-04-26 18:36:33 +0900

    パーサを開発中。

--- test/test_parser.lua
+++ test/test_parser.lua
@@ -15,10 +15,30 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa.  If not, see <https://www.gnu.org/licenses/>.
 
-local json = require "dromozoa.commons.json"
-
 local lexer = require "dromozoa.lexer"
 local parser = require "dromozoa.parser"
 
-local tree = parser.new():parse(lexer.new():lex("1 + 2", "=test"))
-print(json.encode(tree, { pretty = true, stable = true }))
+local function dump(node, buffer)
+  if #node.nodes == 0 then
+    table.insert(buffer, node.kind)
+  else
+    table.insert(buffer, "(")
+    table.insert(buffer, node.kind)
+    for _, node in ipairs(node.nodes) do
+      table.insert(buffer, " ")
+      dump(node, buffer)
+    end
+    table.insert(buffer, ")")
+    return buffer
+  end
+end
+
+local function test(source)
+  local root = assert(parser.new():parse(lexer.new():lex(source, "=test")))
+  return table.concat(dump(root, {}))
+end
+
+print(test "1 and 2 or 3")
+print(test "1 or 2 and 3")
+print(test "1 + 2 + 3")
+print(test "1 ^ 2 ^ 3")
