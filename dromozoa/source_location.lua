d108fb76eadf5db74cc8a04712768fb2a9a66c42
Tomoyuki Fujimori <moyu@dromozoa.com>
2026-05-13 23:27:12 +0900

    リファクタリング。

--- dromozoa/source_location.lua
+++ dromozoa/source_location.lua
@@ -15,8 +15,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa.  If not, see <https://www.gnu.org/licenses/>.
 
-local util = require "dromozoa.util"
-
 ---@class dromozoa.source_location
 ---@field filename string
 ---@field position integer
