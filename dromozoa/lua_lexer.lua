19ebade8195e8d939c664828531e571245ad7279
Tomoyuki Fujimori <moyu@dromozoa.com>
2026-06-19 20:24:47 +0900

    エラーメッセージの作成方法を整理。

--- dromozoa/lua_lexer.lua
+++ dromozoa/lua_lexer.lua
@@ -154,7 +154,7 @@ function class.lex(that)
     value = that._0
   elseif that:match "%-%-%[(=*)%[" then
     if not that:match("(.-)%]" .. that._1 .. "%]") then
-      error(matcher.make_error_string("unfinished long comment", that.start_srcloc))
+      error(that:make_error_string "unfinished long comment")
     end
     kind = "Comment"
     subkind = "Long"
@@ -177,7 +177,7 @@ function class.lex(that)
   end
 
   if not kind or not value then
-    error(matcher.make_error_string("unexpected symbol", that.start_srcloc))
+    error(that:make_error_string "unexpected symbol")
   end
 
   return token.new(kind, subkind, that:substring(start_srcloc), value, start_srcloc, that.last_srcloc)
